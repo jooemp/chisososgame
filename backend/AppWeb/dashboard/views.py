7204398c167b620dd11721fbaba6854e35ba412e
jooemp <jmendoza1292@gmail.com>
2021-09-24 00:48:12 -0500

    duh

--- backend/AppWeb/dashboard/views.py
+++ backend/AppWeb/dashboard/views.py
@@ -5,9 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from core import models
-
 from . import forms
-
 
 @login_required
 def home(request):
